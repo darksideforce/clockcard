5f6ea06f6d7e11f72d7a20afce25b2dfea4a6bbf
mayuemaxiao@qq.com <mayuemaxiao@qq.com>
2023-09-20 00:32:40 +0800

    完成日历部分大致布局

---
+++ src/components/calendar/date.js
@@ -0,0 +1,8 @@
+const dateArray = [
+    [{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''}],
+    [{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''}],
+    [{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''}],
+    [{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''}],
+    [{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''},{day:'',deep:''}],
+]
+export default {dateArray}
